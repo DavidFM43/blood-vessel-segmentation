5ebd1faf1acbe110ece4f10eb63c8a2a009a6336
DavidFM43 <dmorara@unal.edu.co>
2023-12-22 00:18:57 +0000

    Trying to make the metric into a class to use with the dataloader

--- surface_dice.py
+++ surface_dice.py
@@ -10,7 +10,6 @@ from PIL import Image
 # PyTorch version dependence on index data type
 torch_ver_major = int(torch.__version__.split('.')[0])
 dtype_index = torch.int32 if torch_ver_major >= 2 else torch.long
-
 # _NEIGHBOUR_CODE_TO_NORMALS is a lookup table.
 # For every binary neighbour code
 # (2x2x2 neighbourhood = 8 neighbours = 8 bits = 256 codes)
@@ -479,110 +478,71 @@ _NEIGHBOUR_CODE_TO_NORMALS = [[[0, 0, 0]], [[0.125, 0.125, 0.125]],
                               [[-0.125, -0.125, 0.125]],
                               [[0.125, 0.125, 0.125]], [[0, 0, 0]]]
 
-def create_table_neighbour_code_to_surface_area(spacing_mm):
-    """Returns an array mapping neighbourhood code to the surface elements area.
 
-  Note that the normals encode the initial surface area. This function computes
-  the area corresponding to the given `spacing_mm`.
+class SurfaceDiceMetric:
+    def __init__(self, num_batches):
+        self.num_batches = num_batches
+        self.area = torch.from_numpy(self.create_table_neighbour_code_to_surface_area((1, 1, 1)))
+        self.batch_idx = 0
+        self.numerator = 0
+        self.denominator = 0
 
-  Args:
-    spacing_mm: 3-element list-like structure. Voxel spacing in x0, x1 and x2
-      direction.
-  """
-    # compute the area for all 256 possible surface elements
-    # (given a 2x2x2 neighbourhood) according to the spacing_mm
-    neighbour_code_to_surface_area = np.zeros([256])
-    for code in range(256):
-        normals = np.array(_NEIGHBOUR_CODE_TO_NORMALS[code])
-        sum_area = 0
-        for normal_idx in range(normals.shape[0]):
-            # normal vector
-            n = np.zeros([3])
-            n[0] = normals[normal_idx, 0] * spacing_mm[1] * spacing_mm[2]
-            n[1] = normals[normal_idx, 1] * spacing_mm[0] * spacing_mm[2]
-            n[2] = normals[normal_idx, 2] * spacing_mm[0] * spacing_mm[1]
-            area = np.linalg.norm(n)
-            sum_area += area
-        neighbour_code_to_surface_area[code] = sum_area
+    def create_table_neighbour_code_to_surface_area(self, spacing_mm):
+        """Returns an array mapping neighbourhood code to the surface elements area.
 
-    return neighbour_code_to_surface_area.astype(np.float32)
+        Note that the normals encode the initial surface area. This function computes
+        the area corresponding to the given `spacing_mm`.
 
-def compute_area(y: list, unfold: nn.Unfold, area: torch.Tensor, device) -> torch.Tensor:
-    """
-    Args:
-      y (list[Tensor]): A pair of consecutive slices of mask
-      unfold: nn.Unfold(kernel_size=(2, 2), padding=1)
-      area (Tensor): surface area for 256 patterns (256, )
+        Args:
+            spacing_mm: 3-element list-like structure. Voxel spacing in x0, x1 and x2
+            direction.
+        """
+        # compute the area for all 256 possible surface elements
+        # (given a 2x2x2 neighbourhood) according to the spacing_mm
+        neighbour_code_to_surface_area = np.zeros([256])
+        for code in range(256):
+            normals = np.array(_NEIGHBOUR_CODE_TO_NORMALS[code])
+            sum_area = 0
+            for normal_idx in range(normals.shape[0]):
+                # normal vector
+                n = np.zeros([3])
+                n[0] = normals[normal_idx, 0] * spacing_mm[1] * spacing_mm[2]
+                n[1] = normals[normal_idx, 1] * spacing_mm[0] * spacing_mm[2]
+                n[2] = normals[normal_idx, 2] * spacing_mm[0] * spacing_mm[1]
+                area = np.linalg.norm(n)
+                sum_area += area
+            neighbour_code_to_surface_area[code] = sum_area
 
-    Returns:
-      Surface area of surface in 2x2x2 cube
-    """
-    # Two layers of segmentation masks
-    yy = torch.stack(y, dim=0).to(torch.float16).unsqueeze(0)
-    # (batch_size=1, nch=2, H, W) 
-    # bit (0/1) but unfold requires float
+        return neighbour_code_to_surface_area.astype(np.float32)
 
-    # unfold slides through the volume like a convolution
-    # 2x2 kernel returns 8 values (2 channels * 2x2)
-    cubes_float = unfold(yy).squeeze(0)  # (8, n_cubes)
+    def compute_surface_area(self, surface) -> torch.Tensor:
+        unfold = torch.nn.Unfold(kernel_size=(2, 2), padding=1)
+        surface = surface.to(torch.float16).unsqueeze(0)
+        cubes_float = unfold(surface).squeeze(0)
+        cubes_byte = torch.zeros(cubes_float.size(1), dtype=dtype_index, device=device)
+        
+        for k in range(8):
+            cubes_byte += cubes_float[k, :].to(dtype_index) << k
 
-    # Each of the 8 values are either 0 or 1
-    # Convert those 8 bits to one uint8
-    cubes_byte = torch.zeros(cubes_float.size(1), dtype=dtype_index, device=device)
-    # indices are required to be int32 or long for area[cube_byte] below, not uint8
-    # Can be int32 for torch 2.0.0, int32 raise IndexError in torch 1.13.1.
-    
-    for k in range(8):
-        cubes_byte += cubes_float[k, :].to(dtype_index) << k
+        cubes_area = self.area[cubes_byte]
+        return cubes_area
 
-    # Use area lookup table: pattern index -> area [float]
-    cubes_area = area[cubes_byte]
+    def process_batch(pred, target):
+        if self.batch_idx == 0:
+            pred = torch.stack([torch.zeros((h, w), dtype=torch.uint8), pred])
+            target = torch.stack([torch.zeros((h, w), dtype=torch.uint8), target])
+        elif self.batch_idx == num_batches - 1:
+            pred = torch.stack([pred, torch.zeros((h, w), dtype=torch.uint8)])
+            target = torch.stack([target, torch.zeros((h, w), dtype=torch.uint8)])
 
-    return cubes_area
+        area_pred = compute_area(pred)
+        area_true = compute_area(target)
 
-def compute_surface_dice_score(pred: torch.Tensor, target: torch.Tensor, device, return_num_denom=False) -> float:
-    assert pred.shape == target.shape
-
-    # Surface area lookup table: Tensor[float32] (256, )
-    area = create_table_neighbour_code_to_surface_area((1, 1, 1))
-    area = torch.from_numpy(area).to(device)  # torch.float32
-
-    # Slide through the volume like a convolution
-    unfold = torch.nn.Unfold(kernel_size=(2, 2), padding=1)
-
-    n_slices, h, w = pred.shape
-
-    # Padding before first slice
-    y0 = y0_pred = torch.zeros((h, w), dtype=torch.uint8, device=device)
-
-    num = 0     # numerator of surface Dice
-    denom = 0   # denominator
-    for i in range(n_slices + 1):
-        # Load one slice
-        if i < n_slices:
-            y1 = target[i]
-            y1_pred = pred[i]
-        else:
-            # Padding after the last slice
-            y1 = y1_pred = torch.zeros((h, w), dtype=torch.uint8, device=device)
-
-        # Compute the surface area between two slices (n_cubes,)
-        area_pred = compute_area([y0_pred, y1_pred], unfold, area, device=device)
-        area_true = compute_area([y0, y1], unfold, area, device=device)
-
-        # True positive cube indices
         idx = torch.logical_and(area_pred > 0, area_true > 0)
 
-        # Surface dice numerator and denominator
-        num += area_pred[idx].sum() + area_true[idx].sum()
-        denom += area_pred.sum() + area_true.sum()
+        self.num += area_pred[idx].sum() + area_true[idx].sum()
+        self.denom += area_pred.sum() + area_true.sum()
 
-        # Next slice
-        y0 = y1
-        y0_pred = y1_pred
-
-    if return_num_denom:
-        return num, denom
-    else: 
-        dice = num / denom.clamp(min=1e-8)
+    def compute_metric():
+        dice = self.num / self.denom.clamp(min=1e-8)
         return dice.item()
